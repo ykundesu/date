4275b83feab988e687a75053061135da8d2efad2
ykundesu <49261069+ykundesu@users.noreply.github.com>
2020-12-09 20:40:22 +0900

    Update settings.py

--- yoppy/settings.py
+++ yoppy/settings.py
@@ -117,15 +117,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-try:
- STATIC_URL = '/static/'
- PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
- STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
  STATIC_URL = '/static/'
 
- STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
- )
-except:
-    pass
