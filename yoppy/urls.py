86d1611f94ca3c4d79839b051daa5dedd676cc90
ykundesu <49261069+ykundesu@users.noreply.github.com>
2020-12-10 20:50:53 +0900

    Update urls.py

--- yoppy/urls.py
+++ yoppy/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
-from django.conf import settings
+from .settings import * 
 
 admin.site.site_title =  'Yoppy Inc.管理ページ' 
 admin.site.site_header = 'Yoppy Inc.管理ページ'
