698190710743b5f5c1f516828f52db58d2549736
ykundesu <49261069+ykundesu@users.noreply.github.com>
2020-12-11 14:02:15 +0900

    Update settings.py

--- yoppy/settings.py
+++ yoppy/settings.py
@@ -118,4 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # プロジェクト直下のdeployディレクトリを指定
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),  # プロジェクト直下のstaticディレクトリを指定
+)
