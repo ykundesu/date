6f69da70f0bc837db507bbc814fa90043619b399
ykundesu <49261069+ykundesu@users.noreply.github.com>
2020-12-12 15:17:24 +0900

    Update settings.py

--- yoppy/settings.py
+++ yoppy/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'yoppy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'HEROKU_POSTGRESQL_GRAY_URL',
+        'USER': 'yoppy',
+        'PASSWORD': 'Yoppy',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
