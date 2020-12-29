c302c7f6aaecd8e5f4b51b45b7ad7b025b31b3e4
ykundesu <49261069+ykundesu@users.noreply.github.com>
2020-12-29 17:01:39 +0900

    Update settings.py

--- yoppy/settings.py
+++ yoppy/settings.py
@@ -74,12 +74,11 @@ WSGI_APPLICATION = 'yoppy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+import dj_database_url
+db_from_env = dj_database_url.config()
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'default': dj_database_url.config()
+             }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
